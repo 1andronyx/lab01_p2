1c628ad139cad264112f6cf43d07bb00b6af5548
andronyx <andronyx@DESKTOP-RJ9NLN8>
2024-09-17 16:38:07 +0300

    Добавьте файлы проекта.

---
+++ Lr1/Program.cs
@@ -0,0 +1,21 @@
+﻿//task_1
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("byte: min = {0}, max = {1}", byte.MinValue, byte.MaxValue);
+        Console.WriteLine("sbyte: min = {0}, max = {1}", sbyte.MinValue, sbyte.MaxValue);
+        Console.WriteLine("short: min = {0}, max = {1}", short.MinValue, short.MaxValue);
+        Console.WriteLine("ushort: min = {0}, max = {1}", ushort.MinValue, ushort.MaxValue);
+        Console.WriteLine("int: min = {0}, max = {1}", int.MinValue, int.MaxValue);
+        Console.WriteLine("unsigned int: min = {0}, max = {1}", uint.MinValue, uint.MaxValue);
+        Console.WriteLine("long: min = {0}, max = {1}", long.MinValue, long.MaxValue);
+        Console.WriteLine("unsigned_long: min = {0}, max = {1}", ulong.MinValue, ulong.MaxValue);
+        Console.WriteLine("float: min = {0}, max = {1}", float.MinValue, float.MaxValue);
+        Console.WriteLine("double: min = {0}, max = {1}", double.MinValue, double.MaxValue);
+        Console.WriteLine("decimal: min = {0}, max = {1}", decimal.MinValue, decimal.MaxValue);
+    }
+}
+
